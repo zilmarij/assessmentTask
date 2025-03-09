bec5ceb7014970862d80f5d9ce084a1561fb07ec
zilmarij <zilmarij1229@gmail.com>
2025-03-09 15:06:46 +0530

    setup: supabase

---
+++ be/lib/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+export const supabase = createClient(supabaseUrl, supabaseKey);
