5add8cec6bef0378534823add6da1f63a1fd4e42
zilmarij <zilmarij1229@gmail.com>
2025-03-11 12:54:44 +0530

    add: axios setup

--- log-dashboard-fe/src/api/axios.tsx
+++ log-dashboard-fe/src/api/axios.tsx
@@ -1,20 +1,7 @@
-import baseUrl from "@/utils/BaseURL";
 import axios from "axios";
-console.log(baseUrl);
 export const api = axios.create({
   headers: {
     "Access-Control-Allow-Origin": "*",
-    // "Content-Type": "application/json",
+    //"Content-Type": "multipart/form-data",
   },
 });
-
-export const interceptors = () => {
-  api.interceptors.request.use(
-    (config) => {
-      config.baseURL = baseUrl;
-      config.withCredentials = true;
-      return config;
-    },
-    (error) => Promise.reject(error)
-  );
-};
